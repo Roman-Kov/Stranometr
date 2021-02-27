00412971323193fa6cbff70bcfaa249a917eac3e
Roman-Kov <rojer_ko@bk.ru>
2021-02-27 18:31:45 +0300

    Создал новый проект

---
+++ app/src/main/java/com/rojer_ko/stranometr/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rojer_ko.stranometr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
